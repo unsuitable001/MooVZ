6674e83623f9ea428ef2ec8c9ac1b450c43a3c64
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2021-09-14 14:33:30 +0530

    movie card full skeleton & spacing fix

--- lib/components/movie_card.dart
+++ lib/components/movie_card.dart
@@ -9,35 +9,7 @@ class MovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Positioned.fill(
-          child: Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: FractionallySizedBox(
-                widthFactor: 1,
-                heightFactor: 0.7,
-                child: Container(
-                  margin: const EdgeInsets.only(
-                    left: kDefaultPadding,
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: kDefaultPadding,
-                    vertical: kDefaultPadding,
-                  ),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: const <Widget>[Text('ABCD'), Text('ABCD')],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
+        buildInfoCard(context),
         Container(
           margin: const EdgeInsets.only(
             left: 40,
@@ -49,6 +21,69 @@ class MovieCard extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+
+  /// Widget holding the textual information.
+  Widget buildInfoCard(BuildContext context) {
+    return Positioned.fill(
+      child: Container(
+        margin: const EdgeInsets.only(right: kDefaultPadding * 3 / 2),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 0.7,
+            child: Container(
+              margin: const EdgeInsets.only(
+                left: kDefaultPadding,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: kDefaultPadding,
+                vertical: kDefaultPadding,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const <BoxShadow>[kDefaultShadow]),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 120),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text('Movie Title',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            ?.copyWith(fontWeight: FontWeight.w600)),
+                    Text('Movie Categories',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            ?.copyWith(color: kSecondaryFontColor)),
+                    RichText(
+                        text: TextSpan(children: [
+                      const WidgetSpan(
+                          child: Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      )),
+                      TextSpan(
+                          text: '7.1',
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1
+                              ?.copyWith(color: kTextLightColor)),
+                    ])),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
